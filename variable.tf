71e780147bad2afa3e514b97904c90b790d033db
madhavi0891 <66946576+madhavi0891@users.noreply.github.com>
2021-02-17 15:05:01 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,22 @@
+variable "project" {
+  default = "arched-jetty-303107"
+}
+
+variable "region" {
+  default = "us-west2"
+}
+
+variable "zone" {
+  default = "us-west2-c"
+}
+
+variable "cluster" {
+  default = "cicd-workshops"
+}
+
+variable "kubernetes_min_ver" {
+  default = "latest"
+}
+
+variable "kubernetes_max_ver" {
+ 
