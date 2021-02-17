fb7564c3039ecfa2fb50587dde44373de42226c6
madhavi0891 <66946576+madhavi0891@users.noreply.github.com>
2021-02-17 15:04:17 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,30 @@
+resource "google_container_cluster" "primary" {
+  name               = var.cluster
+  location           = var.zone
+  initial_node_count = 3
+  master_auth {
+    username = ""
+    password = ""
+    client_certificate_config {
+      issue_client_certificate = false
+    }
+  }
+  node_config {
+    machine_type = "n1-standard-1"
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
+    metadata = {
+      disable-legacy-endpoints = "true"
+    }
+    labels = {
+      app = "deployment"
+    }
+    tags = ["app", "deployment"]
+  }
+  timeouts {
+    create = "30m"
+    update = "40m"
+  }
+}
